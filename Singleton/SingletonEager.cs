0d0fa31e90f224c4c3e7e9c470940c3b84a8e967
AnnaLviv <annanastasiyalviv@gmail.com>
2020-04-04 01:01:23 +0200

    Singleton: Implemented samples

---
+++ Singleton/SingletonEager.cs
@@ -0,0 +1,22 @@
+﻿namespace Singleton
+{
+    public class SingletonEager
+    {
+        private static SingletonEager uniqueInstance = new SingletonEager();
+        public int IntVariable { get; private set; }
+        private SingletonEager()
+        {
+            IntVariable = 1000;
+        }
+
+        public static SingletonEager GetInstance()
+        {
+            return uniqueInstance;
+        }
+
+        public void SetIntVariable(int newValue)
+        {
+            IntVariable = newValue;
+        }
+    }
+}
---
+++ Singleton/SingletonLazy.cs
@@ -0,0 +1,34 @@
+﻿namespace Singleton
+{
+    public class SingletonLazy
+    {
+        private static SingletonLazy uniqueInstance;
+        private static readonly object lockObject = new object();
+        public int IntVariable { get; private set; }
+        private SingletonLazy() 
+        {
+            IntVariable = 1000;
+        }
+
+        public static SingletonLazy GetInstance()
+        {
+            if(uniqueInstance is null)
+            {
+                lock (lockObject)
+                {
+                    if (uniqueInstance is null)
+                    {
+                        uniqueInstance = new SingletonLazy();
+                    }
+                }
+            }
+
+            return uniqueInstance;
+        }
+
+        public void SetIntVariable(int newValue)
+        {
+            IntVariable = newValue;
+        }
+    }
+}
